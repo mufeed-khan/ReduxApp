7cb094bc5eb5c5f0362a80b5f53d3f7bb63a4ac6
Mufeed Khan <mufeedkhan192@gmail.com>
2023-09-25 15:43:41 +0530

    initail commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Users from "./components/users";
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>welcome redux app</h1>
+      <Users />
+    </div>
+  );
+}
+
+export default App;
